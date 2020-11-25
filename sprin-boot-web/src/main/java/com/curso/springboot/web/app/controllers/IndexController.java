38666471c599b1618f982c89dd0ec0413681fd49
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2020-11-25 06:30:10 +0100

    Creado[ Modelo: Usuario | Vista: Perfil, Listar ]

--- sprin-boot-web/src/main/java/com/curso/springboot/web/app/controllers/IndexController.java
+++ sprin-boot-web/src/main/java/com/curso/springboot/web/app/controllers/IndexController.java
@@ -1,9 +1,14 @@
 package com.curso.springboot.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.springboot.web.app.models.Usuario;
 
 @Controller
 @RequestMapping("/app")
@@ -14,5 +19,27 @@ public class IndexController {
 		model.addAttribute("titulo", "Hola Spring Framework!");
 		return "index";
 	}
+	
+	@GetMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Julen");
+		usuario.setApellido("Rodriguez");
+		usuario.setEmail("julen@pmail.com");
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: " + usuario.getNombre());
 
+		
+		return "perfil";
+	}
+
+	@GetMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<>();
+		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("usuarios", usuarios);
+		
+		return "listar";
+	}
 }
