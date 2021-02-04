e3c3ea8ccb00aaaf2607c8db74c3a2b3676a8b25
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2021-02-04 06:27:18 +0100

    -Fin de la sección 2

--- sprin-boot-web/src/main/java/com/curso/springboot/web/app/controllers/IndexController.java
+++ sprin-boot-web/src/main/java/com/curso/springboot/web/app/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.curso.springboot.web.app.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,18 @@ import com.curso.springboot.web.app.models.Usuario;
 @RequestMapping("/app")
 public class IndexController {
 	
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
+	
 	@GetMapping({"index", "/", "", "home"})
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring Framework!");
+		model.addAttribute("titulo", textoIndex);
 		return "index";
 	}
 	
@@ -29,7 +39,7 @@ public class IndexController {
 		usuario.setEmail("julen@pmail.com");
 		
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil del usuario: " + usuario.getNombre());
+		model.addAttribute("titulo", textoPerfil + usuario.getNombre());
 
 		
 		return "perfil";
@@ -38,7 +48,7 @@ public class IndexController {
 	@GetMapping("/listar")
 	public String listar(Model model) {
 		
-		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("titulo", textoListar);
 		
 		return "listar";
 	}
