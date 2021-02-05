8495c413b7df5f08e658befc8d9e24f38e12ce71
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2021-02-05 06:55:52 +0100

    Implementada la interfaz del servicio.

--- sprin-boot-di/src/main/java/com/curso/springboot/di/app/models/service/MiServicio.java
+++ sprin-boot-di/src/main/java/com/curso/springboot/di/app/models/service/MiServicio.java
@@ -3,8 +3,9 @@ package com.curso.springboot.di.app.models.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MiServicio {
+public class MiServicio  implements IServicio{
 	
+	@Override
 	public String operacion() {
 		return "ejecutando algun proceso importante...";
 	}
