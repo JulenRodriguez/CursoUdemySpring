25387ca7b8ff7b64ac3b1551895677337a940f93
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2021-02-11 06:43:47 +0100

    Inicio de la sección 4 y creación del proyecto "form".

---
+++ sprin-boot-form/src/main/java/com/curso/springboot/form/app/SprinBootFormApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.form.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootFormApplication.class, args);
+	}
+
+}
---
+++ sprin-boot-form/src/test/java/com/curso/springboot/form/app/SprinBootFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.form.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
