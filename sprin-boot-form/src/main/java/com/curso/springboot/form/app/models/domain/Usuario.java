1daf7136f9acc64812a1e0a8a956adb05c1f9935
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2021-03-25 04:46:37 +0100

    Validación con input type date, pasado y futuro.

--- sprin-boot-form/src/main/java/com/curso/springboot/form/app/models/domain/Usuario.java
+++ sprin-boot-form/src/main/java/com/curso/springboot/form/app/models/domain/Usuario.java
@@ -3,6 +3,7 @@ package com.curso.springboot.form.app.models.domain;
 import java.util.Date;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -45,7 +46,8 @@ public class Usuario {
 	private Integer cuenta;
 	
 	@NotNull
-	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	@Future
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date fechaNacimiento;
 
 	
