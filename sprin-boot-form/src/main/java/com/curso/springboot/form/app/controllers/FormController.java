9264bec3572e8fa43d257fdc9e588ef6ee371b42
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2021-03-27 03:42:08 +0100

    Añadido RolePropertyEditor.

--- sprin-boot-form/src/main/java/com/curso/springboot/form/app/controllers/FormController.java
+++ sprin-boot-form/src/main/java/com/curso/springboot/form/app/controllers/FormController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import com.curso.springboot.form.app.editors.NombreMayusculaEditor;
 import com.curso.springboot.form.app.editors.PaisPropertyEditor;
+import com.curso.springboot.form.app.editors.RolesEditor;
 import com.curso.springboot.form.app.models.domain.Pais;
 import com.curso.springboot.form.app.models.domain.Role;
 import com.curso.springboot.form.app.models.domain.Usuario;
@@ -48,6 +49,9 @@ public class FormController {
 	@Autowired
 	private PaisPropertyEditor paisEditor;
 	
+	@Autowired
+	private RolesEditor roleEditor;
+	
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.addValidators(validador);
@@ -59,6 +63,7 @@ public class FormController {
 		binder.registerCustomEditor(String.class, "apellido", new NombreMayusculaEditor());
 		
 		binder.registerCustomEditor(Pais.class, "pais", paisEditor);
+		binder.registerCustomEditor(Role.class, "roles", roleEditor);
 	}
 	
 	@ModelAttribute("listaRoles")
--- sprin-boot-form/src/main/java/com/curso/springboot/form/app/services/RoleServiceImpl.java
+++ sprin-boot-form/src/main/java/com/curso/springboot/form/app/services/RoleServiceImpl.java
@@ -11,24 +11,25 @@ import com.curso.springboot.form.app.models.domain.Role;
 public class RoleServiceImpl implements RoleService{
 
 	private List<Role> roles;
+
 	
-	public RolesServiceImpl() {
+	public RoleServiceImpl() {
 		this.roles = new ArrayList<>();
 		this.roles.add(new Role(1, "Administador", "ROLE_ADMIN"));
 		this.roles.add(new Role(2, "Usuario", "ROLE_USER"));
 		this.roles.add(new Role(3, "Moderador", "ROLE_MODERATOR"));
 		
+		
 	}
 
 	@Override
 	public List<Role> listar() {
-		// TODO Auto-generated method stub
 		return roles;
 	}
 
 	@Override
 	public Role obtenerPorId(Integer id) {
-		for (Role role : roles) {
+		for (Role role : this.roles) {
 			if(id == role.getId()) {
 				return role;
 			}
