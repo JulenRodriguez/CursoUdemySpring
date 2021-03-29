36d81b28c8d975803ddd4f06ba6fc81deb686808
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2021-03-29 04:14:27 +0200

    Lanzamiento de excepción con Api Optional de Java 8.

--- sprin-boot-error/src/main/java/com/curso/springboot/error/app/services/UsuarioServiceImpl.java
+++ sprin-boot-error/src/main/java/com/curso/springboot/error/app/services/UsuarioServiceImpl.java
@@ -2,6 +2,7 @@ package com.curso.springboot.error.app.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -41,6 +42,14 @@ public class UsuarioServiceImpl  implements UsuarioService{
 		}
 		
 		return null;
+	}
+
+	@Override
+	public Optional<Usuario> obtenerPorIdOptional(Integer id) {
+		
+		Usuario usuario = this.obtenerPorId(id);
+		
+		return Optional.ofNullable(usuario);
 	}
 
 }
