4f3eecee0b430f7d37ecbbc0042302754f20c9b2
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2021-03-29 04:01:18 +0200

    Añadido metodo Handler ver.

--- sprin-boot-error/src/main/java/com/curso/springboot/error/app/controllers/AppController.java
+++ sprin-boot-error/src/main/java/com/curso/springboot/error/app/controllers/AppController.java
@@ -1,16 +1,35 @@
 package com.curso.springboot.error.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.curso.springboot.error.app.models.domain.Usuario;
+import com.curso.springboot.error.app.services.UsuarioService;
 
 @Controller
 public class AppController {
 
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	@SuppressWarnings("unused")
 	@GetMapping("/index")
 	public String index() {
 		//Integer valor = 100/0;
 		Integer valor = Integer.parseInt("10xaaa");
 		return "index";
+	}
+	
+	@GetMapping("/ver/{id}")
+	public String ver(@PathVariable Integer id, Model model) {
+		
+		Usuario usuario = usuarioService.obtenerPorId(id);
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Detalle usuario: ".concat(usuario.getNombre()));
+		
+		return "ver";
 	}
 }
--- sprin-boot-error/src/main/java/com/curso/springboot/error/app/controllers/ErrorHandlerController.java
+++ sprin-boot-error/src/main/java/com/curso/springboot/error/app/controllers/ErrorHandlerController.java
@@ -29,4 +29,5 @@ public class ErrorHandlerController {
 		return "error/numero-formato";
 	}
 
+	
 }
