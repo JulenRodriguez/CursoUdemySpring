0bc78705828a4fd81812df1877ee3cedf1934bfd
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2021-03-29 04:24:54 +0200

    Inicio de la sección 8 y nuevo proyecto creado.

---
+++ sprin-boot-data-jpa/src/main/java/com/curso/springboot/app/SprinBootDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootDataJpaApplication.class, args);
+	}
+
+}
