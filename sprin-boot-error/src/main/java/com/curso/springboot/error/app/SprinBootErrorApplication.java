0c5e8a2c36da4b4d0875c2cea18caa9a106c6719
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2021-03-29 02:39:51 +0200

    Creado nuevo proyecto y controlador.

---
+++ sprin-boot-error/src/main/java/com/curso/springboot/error/app/SprinBootErrorApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.error.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootErrorApplication.class, args);
+	}
+
+}
