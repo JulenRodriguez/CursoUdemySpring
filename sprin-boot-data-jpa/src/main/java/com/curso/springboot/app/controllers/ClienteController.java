318666e1adbf1a0dc095071475e4156a08ae2092
Txuflen <Txuflen@DESKTOP-92FJ3R9>
2021-04-07 02:41:18 +0200

    Paginador con PagingAndSortingRepository.

--- sprin-boot-data-jpa/src/main/java/com/curso/springboot/app/controllers/ClienteController.java
+++ sprin-boot-data-jpa/src/main/java/com/curso/springboot/app/controllers/ClienteController.java
@@ -3,6 +3,9 @@ package com.curso.springboot.app.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -25,9 +29,14 @@ public class ClienteController {
 	private ClienteService clienteService;
 	
 	@GetMapping("/listar")
-	public String listar(Model model) {
+	public String listar(@RequestParam (name="page", defaultValue="0") int page, Model model) {
+		
+		Pageable pageRequest = PageRequest.of(page, 4);
+		
+		Page<Cliente> clientes = clienteService.findAll(pageRequest);
+		
 		model.addAttribute("titulo", "Listado de clientes");
-		model.addAttribute("clientes", clienteService.findAll());
+		model.addAttribute("clientes", clientes);
 		return "listar";
 	}
 	
--- sprin-boot-data-jpa/src/main/java/com/curso/springboot/app/models/service/ClienteService.java
+++ sprin-boot-data-jpa/src/main/java/com/curso/springboot/app/models/service/ClienteService.java
@@ -2,12 +2,17 @@ package com.curso.springboot.app.models.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.curso.springboot.app.models.entity.Cliente;
 
 public interface ClienteService {
 
 
 	public List<Cliente> findAll();
+	
+	public Page<Cliente> findAll(Pageable pageable);
 	
 	public void save(Cliente cliente);
 	
